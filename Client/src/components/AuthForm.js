091e2ea19dcb7eb86ecddccde611eac4d5297330
Bijay Bohora <bijayb37@gmail.com>
2022-05-28 14:54:47 +1000

    framer motion

--- Client/src/components/AuthForm.js
+++ Client/src/components/AuthForm.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { authUser } from '../store/actions/authActions'
-import { removeError } from '../store/actions/errorActions'
-import { useMediaQuery } from 'react-responsive'
-import '../styles/authForm.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { authUser } from "../store/actions/authActions"
+import { removeError } from "../store/actions/errorActions"
+import { AnimatePresence, motion } from "framer-motion"
+import "../styles/authForm.css"
+import { Link } from "react-router-dom"
 
 export default function AuthForm(props) {
   const { signup, history } = props
-  const { errors } = useSelector(state => state)
+  const { errors } = useSelector((state) => state)
   const dispatch = useDispatch()
-  const isMobile = useMediaQuery({ maxWidth: 767 }) ? "isMobile" : ""
   const [loading, setLoading] = useState(false)
 
   const [state, setState] = useState({
     username: "",
     password: "",
-    email: ""
+    email: "",
   })
 
   const handleSubmit = (e) => {
@@ -29,7 +28,9 @@ export default function AuthForm(props) {
   const loginGuest = (e) => {
     setLoading(true)
     e.preventDefault()
-    dispatch(authUser("login", { username: "guest", password: "guest" }, setLoading))
+    dispatch(
+      authUser("login", { username: "guest", password: "guest" }, setLoading)
+    )
   }
 
   const handleChange = (e) => {
@@ -41,45 +42,127 @@ export default function AuthForm(props) {
   })
   return (
     <div className="authFormContainer">
-      <div className={`signupBox ${isMobile}`}>
-        <form className="authForm" >
-          <div className="loginInputs">
-            {signup
-              ? <span className="inputTitle" >Sign Up</span>
-              : <span className="inputTitle" >Login to your account</span>
-            }
-            {signup &&
-              <div className="mb-3 emptySpace" >
-                <label htmlFor="email" className="form-label">Email Address</label>
-                <input onChange={handleChange} type="text" className="form-control" name="email" placeholder="name@example.com" />
-              </div>
-            }
-            <div className="mb-3 emptySpace" >
-              <label htmlFor="username" className="form-label">Username</label>
-              <input onChange={handleChange} type="text" className="form-control" name="username" />
-            </div>
-            <div className="mb-3 emptySpace" >
-              <label htmlFor="password" className="form-label">Password</label>
-              <input onChange={handleChange} type="password" className="form-control" name="password" />
-            </div>
-          </div>
-          <div className="loginButtons emptySpace">
-            <button onClick={handleSubmit} type="submit" className="btn btn-primary">{signup ? "Sign Up" : "Log In"}</button>
-            {!signup && <span onClick={loginGuest}>Login As Guest</span>}
-          </div>
-        </form>
-      </div>
-      {!signup && <span className="signupPrompt">Don't have an account <Link to="/signup">Sign Up?</Link></span>}
-      {signup && <span className="signupPrompt">Already have an account <Link to="/login">Log In?</Link></span>}
-      {loading && <>
-        <div className="spinner-border" role="status"></div>
-        <span>Loading...</span>
-      </>}
-      {errors.message &&
-        <div className="alert alert-danger" role="alert">
-          {errors.message}
+      <div className="formBox d-flex align-items-center">
+        <div className="signupBox bg-glass">
+          <AnimatePresence>
+            <motion.form className="authForm">
+              {signup ? (
+                <motion.div
+                  className="loginInputs"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                  key={1}
+                >
+                  <span className="inputTitle">Sign Up</span>
+                  <div className="mb-3 emptySpace">
+                    <label htmlFor="email" className="form-label">
+                      Email Address
+                    </label>
+                    <input
+                      onChange={handleChange}
+                      type="text"
+                      className="form-control"
+                      name="email"
+                      placeholder="name@example.com"
+                    />
+                  </div>
+                  <div className="mb-3 emptySpace">
+                    <label htmlFor="username" className="form-label">
+                      Username
+                    </label>
+                    <input
+                      onChange={handleChange}
+                      type="text"
+                      className="form-control"
+                      name="username"
+                    />
+                  </div>
+                  <div className="mb-3 emptySpace">
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      onChange={handleChange}
+                      type="password"
+                      className="form-control"
+                      name="password"
+                    />
+                  </div>
+                  <div className="loginButtons emptySpace">
+                    <button
+                      onClick={handleSubmit}
+                      type="submit"
+                      className="btn btn-primary"
+                    >
+                      Sign Up
+                    </button>
+                  </div>
+                  <span className="signupPrompt">
+                    Already have an account <Link to="/login">Log In?</Link>
+                  </span>
+                </motion.div>
+              ) : (
+                <motion.div
+                  className="loginInputs"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                  key={2}
+                >
+                  <span className="inputTitle">Login to your account</span>
+                  <div className="mb-3 emptySpace">
+                    <label htmlFor="username" className="form-label">
+                      Username
+                    </label>
+                    <input
+                      onChange={handleChange}
+                      type="text"
+                      className="form-control"
+                      name="username"
+                    />
+                  </div>
+                  <div className="mb-3 emptySpace">
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      onChange={handleChange}
+                      type="password"
+                      className="form-control"
+                      name="password"
+                    />
+                  </div>
+                  <div className="loginButtons emptySpace">
+                    <button
+                      onClick={handleSubmit}
+                      type="submit"
+                      className="btn btn-primary"
+                    >
+                      Log In
+                    </button>
+                    <span onClick={loginGuest}>Login As Guest</span>
+                  </div>
+                  <span className="signupPrompt">
+                    Don't have an account <Link to="/signup">Sign Up?</Link>
+                  </span>
+                </motion.div>
+              )}
+            </motion.form>
+          </AnimatePresence>
         </div>
-      }
+        {loading && (
+          <>
+            <div className="spinner-border" role="status"></div>
+            <span>Loading...</span>
+          </>
+        )}
+        {errors.message && (
+          <div className="alert alert-danger" role="alert">
+            {errors.message}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
